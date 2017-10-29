490d99397154fd0a0e39248bda81bb1b9b30a224
Mgrev <tibidrake@gmail.com>
2017-10-29 23:31:10 +0300

    fixed something

--- bmbotscript.py
+++ bmbotscript.py
@@ -56,14 +56,14 @@ def main():
 			today+=1
 			
 		elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            today += 1
-
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            today += 1
+			greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+			today += 1
+			
+		elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+			greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+			today += 1      
             
-          new_offset = last_update_id + 1
+		new_offset = last_update_id + 1
           
 
 if __name__ == '__main__':
